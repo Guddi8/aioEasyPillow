f5650f6b83c120ed0312beaa1d5918debe51d7b8
Guddi <niedchri@gmail.com>
2022-06-26 00:33:25 +0200

    add imports

--- aioEasyPillow/__init__.py
+++ aioEasyPillow/__init__.py
@@ -17,6 +17,12 @@ __version__ = '0.0.1a1'
 
 from typing import NamedTuple, Literal
 
+from .canvas import Canvas
+from .editor import Editor
+from .font import Font
+from .text import Text
+from .utils import load_image, load_image_async, run_in_executor
+
 
 class VersionInfo(NamedTuple):
     major: int
