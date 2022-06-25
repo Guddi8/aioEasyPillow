5cc09cda62ccf77740bf6fd3031b26575ba63ac3
Guddi <niedchri@gmail.com>
2022-06-26 00:15:35 +0200

    added information

--- aioEasyPillow/__init__.py
+++ aioEasyPillow/__init__.py
@@ -1,4 +1,31 @@
 """
+AsyncIO Easy Pillow
+~~~~~~~~~~~~~~~~~~~
 
+Async working easy to use Pillow Library.
+
+:copyright: (c) 2021-2022 shahriyardx & (c) 2022-present Guddi
+:license: MIT, see LICENSE for more details.
 
 """
+
+__title__ = 'aioEasyPillow'
+__author__ = 'Guddi'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-2022 shahriyardx & 2022-present Guddi'
+__version__ = '0.0.1a1'
+
+from typing import NamedTuple, Literal
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    release: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=0, minor=0, micro=1, release='alpha', serial=1)
+
+del NamedTuple, Literal, VersionInfo
